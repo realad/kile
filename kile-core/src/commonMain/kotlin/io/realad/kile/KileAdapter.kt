29f8647c84957704130602876605f9f5f9aa1ba1
Andrew Kochura <alexeevi40327@gmail.com>
2020-07-02 13:58:14 +0300

    Export KileAdapter to js package.

--- kile-core/src/commonMain/kotlin/io/realad/kile/KileAdapter.kt
+++ kile-core/src/commonMain/kotlin/io/realad/kile/KileAdapter.kt
@@ -2,10 +2,14 @@ package io.realad.kile
 
 import io.realad.kile.error.FilesystemError
 import io.realad.kile.fp.Either
+import kotlin.js.ExperimentalJsExport
+import kotlin.js.JsExport
 
 /**
  * Base interface for any file system.
  */
+@ExperimentalJsExport
+@JsExport
 interface KileAdapter {
 
     /**
